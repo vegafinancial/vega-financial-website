89858a98faf442c84be160b59e174d8665366a6b
Yilu <yilu@vega-financial.com>
2026-07-08 16:37:54 -0700

    Add sitemap generation, robots.txt; sitemap.xml convenience redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://www.vega-financial.com',
   output: 'static',
+  integrations: [sitemap()],
 });
